fbf03eb1929cd4a5867e54e51be3ed6fc2365283
LinCpj <truelin@mofox.cn>
2016-05-05 14:18:34 +0800

    加框架包名

--- app/src/main/java/cn/myasapp/main/MainActivity.java
+++ app/src/main/java/cn/myasapp/main/MainActivity.java
@@ -1,9 +1,11 @@
-package cn.myasapp;
+package cn.myasapp.main;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import cn.myasapp.R;
 
 
 public class MainActivity extends ActionBarActivity {
