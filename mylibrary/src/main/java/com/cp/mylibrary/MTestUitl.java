32d0a416c8243d6ef1cdf5fc8787ec83676c4a6a
lin <467471641@qq.com>
2016-07-03 15:56:00 +0800

    测试自己建library

---
+++ mylibrary/src/main/java/com/cp/mylibrary/MTestUitl.java
@@ -0,0 +1,13 @@
+package com.cp.mylibrary;
+
+/**
+ * Created by lin on 7/3/2016.
+ */
+public class MTestUitl {
+
+     public static void testCp()
+     {
+         System.out.print(" 林楚萍,,,");
+     }
+
+}
