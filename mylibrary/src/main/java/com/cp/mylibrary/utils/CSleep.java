f997cbb192dc3c7d1f87f024dd7f5e1f2cd9f132
467471641@qq.com <467471641@qq.com>
2016-07-05 17:04:10 +0800

    添加异常处理类

---
+++ mylibrary/src/main/java/com/cp/mylibrary/utils/CSleep.java
@@ -0,0 +1,37 @@
+package com.cp.mylibrary.utils;
+
+/**
+ * Created by Jerry on 2016/7/5.
+ *
+ * 线程睡眠
+ *
+ */
+public class CSleep {
+    public static final long DEFAULT_SLEEP_TIME = 500;
+
+    private boolean          isRuning           = false;
+
+    public boolean isRuning() {
+        return isRuning;
+    }
+
+    public void runWithTime(final long defaultSleepTime) {
+        isRuning = true;
+        new Thread() {
+
+            @Override
+            public void run() {
+                try {
+                    sleep(defaultSleepTime, 0);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                isRuning = false;
+                super.run();
+            }
+        }.start();
+    }
+
+
+
+}
