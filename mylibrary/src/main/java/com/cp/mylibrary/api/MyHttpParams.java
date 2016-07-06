7a8d253edc90694c605914f312c6a3d5d37b3300
467471641@qq.com <467471641@qq.com>
2016-07-06 09:10:04 +0800

    封装http

---
+++ mylibrary/src/main/java/com/cp/mylibrary/api/MyHttpParams.java
@@ -0,0 +1,10 @@
+package com.cp.mylibrary.api;
+
+import org.kymjs.kjframe.http.HttpParams;
+
+/**
+ * Created by Jerry on 2016/7/6.
+ *  http 请求参数
+ */
+public class MyHttpParams extends HttpParams {
+}
