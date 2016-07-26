06d6361a0afce9f07d71ec96d35c5f3f88db02de
467471641@qq.com <467471641@qq.com>
2016-07-26 16:20:23 +0800

    添加time

--- mylibrary/src/main/java/com/cp/mylibrary/utils/StringUtils.java
+++ mylibrary/src/main/java/com/cp/mylibrary/utils/StringUtils.java
@@ -36,14 +36,16 @@ public class StringUtils {
     public static boolean isEmpty(String input) {
         if (input == null || "".equals(input))
             return true;
+        else
+            return false;
 
-        for (int i = 0; i < input.length(); i++) {
-            char c = input.charAt(i);
-            if (c != ' ' && c != '\t' && c != '\r' && c != '\n') {
-                return false;
-            }
-        }
-        return true;
+//        for (int i = 0; i < input.length(); i++) {
+//            char c = input.charAt(i);
+//            if (c != ' ' && c != '\t' && c != '\r' && c != '\n') {
+//                return false;
+//            }
+//        }
+//        return true;
     }
 
     /**
