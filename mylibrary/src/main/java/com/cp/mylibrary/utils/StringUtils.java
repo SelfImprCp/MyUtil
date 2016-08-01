8bfdb0044a207730e785b147c563a8362cee5af7
467471641@qq.com <467471641@qq.com>
2016-08-01 09:05:48 +0800

    换http

--- mylibrary/src/main/java/com/cp/mylibrary/utils/StringUtils.java
+++ mylibrary/src/main/java/com/cp/mylibrary/utils/StringUtils.java
@@ -104,6 +104,48 @@ public class StringUtils {
         return URL.matcher(str).matches();
     }
 
+
+    /**
+     * 判断是否是银行卡号
+     * @param cardId
+     * @return
+     */
+    public static boolean checkBankCard(String cardId) {
+        char bit = getBankCardCheckCode(cardId
+                .substring(0, cardId.length() - 1));
+        if (bit == 'N') {
+            return false;
+        }
+        return cardId.charAt(cardId.length() - 1) == bit;
+
+    }
+
+    private static char getBankCardCheckCode(String nonCheckCodeCardId) {
+        if (nonCheckCodeCardId == null
+                || nonCheckCodeCardId.trim().length() == 0
+                || !nonCheckCodeCardId.matches("\\d+")) {
+            // 如果传的不是数据返回N
+            return 'N';
+        }
+        char[] chs = nonCheckCodeCardId.trim().toCharArray();
+        int luhmSum = 0;
+        for (int i = chs.length - 1, j = 0; i >= 0; i--, j++) {
+            int k = chs[i] - '0';
+            if (j % 2 == 0) {
+                k *= 2;
+                k = k / 10 + k % 10;
+            }
+            luhmSum += k;
+        }
+        return (luhmSum % 10 == 0) ? '0' : (char) ((10 - luhmSum % 10) + '0');
+    }
+
+
+
+
+
+
+
     /**
      * pass
      * 字符串转整数
