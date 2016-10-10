219fd8aa76446de9b8eb98a81d3f558d0ddb33c1
467471641@qq.com <467471641@qq.com>
2016-10-10 11:34:55 +0800

    添加热补丁

--- mylibrary/src/main/java/com/cp/mylibrary/app/MyBaseApp.java
+++ mylibrary/src/main/java/com/cp/mylibrary/app/MyBaseApp.java
@@ -4,7 +4,7 @@ import android.app.Application;
 import android.os.Environment;
 import android.util.Log;
 
-import com.alipay.euler.andfix.patch.PatchManager;
+
 import com.cp.mylibrary.utils.ActivityManagerUtil;
 
 import java.io.File;
@@ -22,7 +22,7 @@ public class MyBaseApp extends Application {
     /**
      * patch manager
      */
-    private PatchManager mPatchManager;
+//    private PatchManager mPatchManager;
 //
 //
 
@@ -43,32 +43,35 @@ public class MyBaseApp extends Application {
 
     private void addPatchManager() {
         // initialize
-        mPatchManager = new PatchManager(this);
-        mPatchManager.init("1.0");
-        Log.d(TAG, "inited.");
+//        mPatchManager = new PatchManager(this);
+//        mPatchManager.init("1.0");
+//        Log.d(TAG, "inited.");
+//
+//        // load patch
+//        mPatchManager.loadPatch();
+//        Log.d(TAG, "apatch loaded.");
+//
+//        // add patch at runtime
+//        try {
+//            // .apatch file path
+//            String patchFileString = Config.DEFAULT_SAVE_FILE_PATH + Config.APATCH_NAME;
+//            mPatchManager.addPatch(patchFileString);
+//            Log.d(TAG, "apatch:" + patchFileString + " added.");
+//            //这里添加一个方法 ，复制补丁成功后，删除sdCard的补丁，避免每次进入程序都重新加载一次
+//            File f = new File(this.getFilesDir(), Config.DEFAULT_SAVE_FILE_PATH + Config.APATCH_NAME);
+//            if (f.exists()) {
+//                boolean result = new File(patchFileString).delete();
+//                if (!result)
+//                    Log.e(TAG, patchFileString + " delete fail");
+//            }
+//
+//
+//        } catch (IOException e) {
+//            Log.e(TAG, "", e);
+//        }
+//
 
-        // load patch
-        mPatchManager.loadPatch();
-        Log.d(TAG, "apatch loaded.");
 
-        // add patch at runtime
-        try {
-            // .apatch file path
-            String patchFileString = Config.DEFAULT_SAVE_FILE_PATH + Config.APATCH_NAME;
-            mPatchManager.addPatch(patchFileString);
-            Log.d(TAG, "apatch:" + patchFileString + " added.");
-            //这里添加一个方法 ，复制补丁成功后，删除sdCard的补丁，避免每次进入程序都重新加载一次
-            File f = new File(this.getFilesDir(), Config.DEFAULT_SAVE_FILE_PATH + Config.APATCH_NAME);
-            if (f.exists()) {
-                boolean result = new File(patchFileString).delete();
-                if (!result)
-                    Log.e(TAG, patchFileString + " delete fail");
-            }
-
-
-        } catch (IOException e) {
-            Log.e(TAG, "", e);
-        }
 
     }
 
