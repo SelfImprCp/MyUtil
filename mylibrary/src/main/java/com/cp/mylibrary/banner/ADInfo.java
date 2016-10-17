ca0c44bf8693263bb98ad3ad56edce823bdda0ee
467471641@qq.com <467471641@qq.com>
2016-10-17 17:07:45 +0800

    添加

--- mylibrary/src/main/java/com/cp/mylibrary/banner/ADInfo.java
+++ mylibrary/src/main/java/com/cp/mylibrary/banner/ADInfo.java
@@ -19,6 +19,15 @@ public class ADInfo {
 	private String shareTitle;
 	 private  String share_url ;
 
+
+	public String getShare_url() {
+		return share_url;
+	}
+
+	public void setShare_url(String share_url) {
+		this.share_url = share_url;
+	}
+
 	public String getImgUrl() {
 		return imgUrl;
 	}
