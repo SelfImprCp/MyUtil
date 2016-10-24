44249c19e8ddfec92ef711cc2b42d495d313d78d
467471641@qq.com <467471641@qq.com>
2016-10-24 16:47:44 +0800

    添加

--- mylibrary/src/main/java/com/cp/mylibrary/base/MyBaseActivity.java
+++ mylibrary/src/main/java/com/cp/mylibrary/base/MyBaseActivity.java
@@ -16,6 +16,7 @@ import com.cp.mylibrary.app.MyBaseApp;
 
 import com.cp.mylibrary.event.BaseEvent;
 import com.cp.mylibrary.utils.ActivityManagerUtil;
+import com.cp.mylibrary.utils.AppUtils;
 import com.cp.mylibrary.utils.LogCp;
 import com.cp.mylibrary.utils.NetWorkUtil;
 import com.cp.mylibrary.utils.ShowToastUtil;
@@ -38,9 +39,6 @@ public class MyBaseActivity extends KJActivity {
 
 
 
-    private static final int MY_PERMISSIONS_REQUEST_WRITE_EXTERNAL_STORAGE = 1;
-
-    private static final int MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE =2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -75,7 +73,7 @@ public class MyBaseActivity extends KJActivity {
 
 
 
-        getPromission();
+ AppUtils. getPromission(this);
 
 
 
@@ -184,72 +182,6 @@ public class MyBaseActivity extends KJActivity {
         super.onStop();
     }
 
-    /**
-     * 检查权限
-     */
-    private  void getPromission()
-    {
-        LogCp.i(LogCp.CP, "  来取权限!");
-
-
-
-
-//        if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)
-//                != PackageManager.PERMISSION_GRANTED) {
-//            //申请WRITE_EXTERNAL_STORAGE权限
-//            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
-//                    WRITE_EXTERNAL_STORAGE_REQUEST_CODE);
-//        }
-//
-
-
-        // 读取sdcard 权限
-        if (PackageManager.PERMISSION_GRANTED == ContextCompat.checkSelfPermission(MyBaseActivity.this, Manifest.permission.READ_EXTERNAL_STORAGE)) {
-
-            LogCp.i(LogCp.CP, " 已经有了读取sdcard 的权限!");
-        } else {
-            //do not have permission
-            LogCp.i(LogCp.CP, "  没有 读取sdcard 的权限!");
-            // Should we show an explanation?
-            if (ActivityCompat.shouldShowRequestPermissionRationale(MyBaseActivity.this,
-                    Manifest.permission.READ_EXTERNAL_STORAGE)) {
-
-
-            } else {
-
-                ActivityCompat.requestPermissions(MyBaseActivity.this,
-                        new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
-                        MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE);
-            }
-        }
-
-
-
-        // 写入sdcard 权限
-        if (PackageManager.PERMISSION_GRANTED == ContextCompat.checkSelfPermission(MyBaseActivity.this, Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
-
-            LogCp.i(LogCp.CP, " 已经有了写入sdcard 的权限!");
-        } else {
-            //do not have permission
-            LogCp.i(LogCp.CP, "  没有 写入sdcard 的权限!");
-            // Should we show an explanation?
-            if (ActivityCompat.shouldShowRequestPermissionRationale(MyBaseActivity.this,
-                    Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
-
-
-            } else {
-
-
-                ActivityCompat.requestPermissions(MyBaseActivity.this,
-                        new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
-                        MY_PERMISSIONS_REQUEST_WRITE_EXTERNAL_STORAGE);
-
-
-
-            }
-        }
-
-    }
 
 
 
--- mylibrary/src/main/java/com/cp/mylibrary/utils/AppUtils.java
+++ mylibrary/src/main/java/com/cp/mylibrary/utils/AppUtils.java
@@ -1,8 +1,14 @@
 package com.cp.mylibrary.utils;
 
+import android.Manifest;
+import android.app.Activity;
 import android.content.Context;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+
+import com.cp.mylibrary.base.MyBaseActivity;
 
 /**
  *
@@ -11,6 +17,9 @@ import android.content.pm.PackageManager;
  */
 public class AppUtils {
 
+    private static final int MY_PERMISSIONS_REQUEST_WRITE_EXTERNAL_STORAGE = 1;
+
+    private static final int MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE =2;
 
 
 
@@ -23,6 +32,78 @@ public class AppUtils {
      {
           return context.getPackageName();
      }
+
+
+
+
+    /**
+     * 检查权限
+     */
+    public static   void getPromission(Activity context)
+    {
+        LogCp.i(LogCp.CP,    AppUtils .class + "  来取权限!");
+
+
+
+
+//        if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+//                != PackageManager.PERMISSION_GRANTED) {
+//            //申请WRITE_EXTERNAL_STORAGE权限
+//            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+//                    WRITE_EXTERNAL_STORAGE_REQUEST_CODE);
+//        }
+//
+
+
+        // 读取sdcard 权限
+        if (PackageManager.PERMISSION_GRANTED == ContextCompat.checkSelfPermission(context, Manifest.permission.READ_EXTERNAL_STORAGE)) {
+
+            LogCp.i(LogCp.CP, " 已经有了读取sdcard 的权限!");
+        } else {
+            //do not have permission
+            LogCp.i(LogCp.CP, "  没有 读取sdcard 的权限!");
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(context,
+                    Manifest.permission.READ_EXTERNAL_STORAGE)) {
+
+
+            } else {
+
+                ActivityCompat.requestPermissions(context,
+                        new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+                        MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE);
+            }
+        }
+
+
+
+        // 写入sdcard 权限
+        if (PackageManager.PERMISSION_GRANTED == ContextCompat.checkSelfPermission(context, Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+
+            LogCp.i(LogCp.CP, " 已经有了写入sdcard 的权限!");
+        } else {
+            //do not have permission
+            LogCp.i(LogCp.CP, "  没有 写入sdcard 的权限!");
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(context,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+
+
+            } else {
+
+
+                ActivityCompat.requestPermissions(context,
+                        new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                        MY_PERMISSIONS_REQUEST_WRITE_EXTERNAL_STORAGE);
+
+
+
+            }
+        }
+
+    }
+
+
 
 
 }
